a8e465727bb06632ace14b8ee33fa7d62437ef91
RVINDEERA <rdasbandara@gmail.com>
2023-12-22 12:39:31 +0530

    Complete DAOFactory

--- src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
@@ -1,9 +1,10 @@
 package com.example.layeredarchitecture.dao.custom;
 
+import com.example.layeredarchitecture.dao.SupperDAO;
 import com.example.layeredarchitecture.model.PlacedOrdersDTO;
 
 import java.sql.SQLException;
 
-public interface QueryDAO {
+public interface QueryDAO extends SupperDAO {
     PlacedOrdersDTO printPlacedOrdersDetails() throws SQLException, ClassNotFoundException;
 }
